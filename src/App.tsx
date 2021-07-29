85146024e99a79dda7f6126012aaed7e006fe435
DC Cunningham <dc@beautifulrevolution.com.au>
2021-07-29 20:55:49 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { myContext } from './Context'
+import Homepage from './Components/Homepage/Homepage';
+import LoginPage from './Components/LoginPage/LoginPage';
+import NavBar from './Components/NavBar/NavBar';
+import "./GlobalStyles.css"
+
+function App() {
+  const userObject = useContext(myContext);
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+        {
+          userObject ? null : (
+            <Route path='/login' component={LoginPage} />
+          )
+        }
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
